9451b53406f2f801d83aa6e67e9cf12e77e3bba6
Riza Mirza <skyline.summery@gmail.com>
2023-06-27 12:14:29 +0700

    Create auto-restart.sh

---
+++ auto-restart.sh
@@ -0,0 +1,15 @@
+#skrip cek service down
+
+#bila db mysql mati, hidupkan
+systemctl status mysql | grep 'active (running)' > /dev/null 2>&1
+if [ $? != 0 ]
+then
+systemctl restart mysql> /dev/null
+fi
+
+#bila nginx mati, hidupkan
+systemctl status nginx | grep 'active (running)' > /dev/null 2>&1
+if [ $? != 0 ]
+then
+sudo systemctl restart  nginx > /dev/null
+fi
